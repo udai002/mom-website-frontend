315118efb2322e78dbc20b0d41c5f1ec5bb53b54
pushpa <pushpasenorita@gmail.com>
2025-08-22 19:20:31 +0530

    updated modal and export

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -2,6 +2,7 @@ import React ,{useState} from 'react'
 import ContactUs from '../components/Users.jsx/ContactUs'
 import GetEarlyAccess from '../components/Users.jsx/GetEarlyAccess'
 import Modal from '../components/Users.jsx/Modal'
+import ExportPDF from '../components/pdf'
 function Users() {
 const[showModal , setShowModal] = useState(false)
 
@@ -9,8 +10,10 @@ const[showModal , setShowModal] = useState(false)
     <div >
         <button onClick={()=>setShowModal(true)} className='font-bold text-[red] bg-black py-2 px-3 items-end float-right'  >getEarly aceess</button>
         {showModal && <Modal onClose={()=>setShowModal(false)}/>}
+             <ExportPDF elementId="users" fileName="Users.pdf" /> 
+            <div id="users">
         <ContactUs />
-
+</div>
     </div>
   )
 }
