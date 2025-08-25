bbbe6b06dd6ce12ab1e2558ebf65d009cc291b36
akhiladharavath <106368196+akhiladharavath@users.noreply.github.com>
2025-08-25 14:04:28 +0530

    updated

--- src/components/Navigation/Button.tsx
+++ src/components/Navigation/Button.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router";
+import { Store } from "../../context/NavBarContext";
 
 type ButtonProps = {
   icon: React.ReactNode;
@@ -9,12 +10,32 @@ type ButtonProps = {
 };
 
 function Button(props: ButtonProps) {
+  const context = useContext(Store);
+  if (!context) {
+    throw new Error("Sidenavbar must be used within a Store.Provider");
+  }
+  const { open, setOpen } = context;
+
+  console.log("..........", open);
+
+  function handleOpenOptions() {
+    setOpen(true);
+  }
+
   const content = (
     <div className="flex items-center gap-5 p-1">
-      {props.icon && <span className="">{props.icon}</span>}
-      <p className="">{props.title}</p>
+      {props.icon && <span>{props.icon}</span>}
+      <p>{props.title}</p>
     </div>
   );
+
+  const icons = (
+    <div className="flex items-center gap-5 p-1">
+      {props.icon && <span>{props.icon}</span>}
+    </div>
+  );
+
+  const innerContent = open ? content : icons;
 
   return (
     <div className="flex flex-col">
@@ -23,13 +44,14 @@ function Button(props: ButtonProps) {
           to={props.link}
           className={`flex items-center justify-between px-2 text-white border border-[#c2c2c2] m-1 mt-4 rounded-[10px] py-2 bg-[#00a99d] transition-colors duration-200 hover:bg-white hover:text-[#00a99d] ${props.className}`}
         >
-          {content}
+          {innerContent}
         </Link>
       ) : (
         <button
+          onClick={handleOpenOptions}
           className={`flex items-center justify-between text-white px-2 border border-[#c2c2c2] m-1 mt-4 rounded-[10px] py-2 bg-[#00a99d] transition-colors duration-200 hover:bg-white hover:text-[#00a99d] ${props.className}`}
         >
-          {content}
+          {innerContent}
         </button>
       )}
     </div>
