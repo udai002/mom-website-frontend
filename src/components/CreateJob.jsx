2c291c2e718ca6ff3d8bceac7200cd66c1c58092
chandu <chandunatakarani@gmail.com>
2025-08-26 14:38:24 +0530

    job form

---
+++ src/components/CreateJob.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CreateJob = () => {
+  return (
+    <>
+    <div>
+        <h1 className=''>Create job+</h1>
+    </div>
+    </>
+  )
+}
+
+export default CreateJob
