967d50716dbb39877df1de28afb4141fec1ead86
adarsh745 <adarshjanjirala745@gmail.in>
2025-09-05 17:39:14 +0530

    commited LeavApply

--- src/components/LeavesApply.jsx
+++ src/components/LeavesApply.jsx
@@ -12,25 +12,34 @@ import apiClient from "../utils/apliClent";
 import toast from "react-hot-toast";
 import { FcApproval } from "react-icons/fc";
 import { MdCancel } from "react-icons/md";
+import { body } from "framer-motion/client";
+
+const statusColors  = {
+  Pending:'#F5C983' , 
+  Approved:"#A8F583" , 
+  Cancelled:"#F08080"
+}
 
 
 function LeavesApply() {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(1);
-  const [limit] = useState(6);
+  const [limit] = useState(5);
   const [totalPages, setTotalPages] = useState(0);
   const [totalResponses, setTotalResponses] = useState(0);
   const [selectedRows, setSelectedRows] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [editingEmployee, setEditingEmployee] = useState(null);
   const[colorstate , setColor]=useState("")
+  const[disabled,setDisabled]=useState(false)
+  const [renderer, setRenderer] = useState('')
 
   const navigate = useNavigate();
     //   `employee/allemployees?search=${search}&page=${page}&limit=${limit}`
 
   const fetchEmployees = () => {
-    apiClient("api/leaves")
+    apiClient(`api/leaves?search=${search}&page=${page}&limit=${limit}`)
     
       .then((data) => {
         setData(data.data);
@@ -44,23 +53,32 @@ function LeavesApply() {
 
   useEffect(() => {
     fetchEmployees();
-  }, [search, page, limit]);
+  }, [search, page, limit , renderer ]);
 
-  const handleAproved = async (id, key) => {
-    console.log(id);
+  const handleAproved = async (id, key,email) => {
+    console.log("email....",id,email);
     if (!window.confirm("Are you sure you want to Approve leave for this employee?"))
       return;
     console.log(id, key);
+
 // http://localhost:3000/api/leave/approve/68b842634ccee244e2a18e34
     try {
       const res = await apiClient(`api/leave/approve/${id}`, {
         method: "PUT",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify({email})
       });
-      const result = await res.json();
+      // const result = await res.json();
       if (result) {
-        setData((prev) => prev.filter((emp) => emp._id !== id));
+        // setData((prev) => prev.filter((emp) => emp._id !== id));
         // alert("Deleted successfully");
         toast.success("Approved successfully");
+        console.log("this is approved leave" , data)
+        setData(prev=>prev.map(item=>item._id==id?{...item , status:"Approved"}:item))
+        setRenderer(Date.now())
+        setDisabled(true)
       } else {
         alert("Failed to Approve");
       }
@@ -121,6 +139,8 @@ function LeavesApply() {
   {
        if (!window.confirm("Are you sure you want to cancel?"))
         return
+                      setDisabled(true)
+
 
        console.log("id n key",id,key)
 
@@ -130,9 +150,12 @@ function LeavesApply() {
       });
       const result = await res.json();
       if (result) {
+        
         setData((prev) => prev.filter((emp) => emp._id !== id));
         // alert("Deleted successfully");
+        setRenderer(Date.now())
         toast.success("Cancele successfully");
+
       } else {
         alert("Failed to Cancel");
       }
@@ -149,68 +172,86 @@ function LeavesApply() {
   function handleChange(e)
   {
      const {name,value}=e.target
+     console.log(".......... ausbdsusududb",colorstate)
 
   }
 
   const columns = [
     {
       id: "select",
-      header: (
-        <input
-          type="checkbox"
-          className="w-5 h-5 rounded border-2 border-[#00a99d] peer-checked:bg-[#e0f7f5] flex items-center justify-center"
-          onChange={(e) => {
-            if (e.target.checked) {
-              setSelectedRows(data.map((row) => row._id));
-            } else {
-              setSelectedRows([]);
-            }
-          }}
-          checked={data.length > 0 && selectedRows.length === data.length}
-        />
-      ),
-      cell: (row) => (
-        <input
-          type="checkbox"
-          checked={selectedRows.includes(row._id)}
-          className="w-5 h-5 rounded border-2 border-[#00a99d] peer-checked:bg-[#e0f7f5] flex items-center justify-center"
-          onChange={(e) => {
-            if (e.target.checked) {
-              setSelectedRows([...selectedRows, row._id]);
-            } else {
-              setSelectedRows(selectedRows.filter((id) => id !== row._id));
-            }
-          }}
-        />
-      ),
+      // header: (
+      //   <input
+      //     type="checkbox"
+      //     className="w-5 h-5 rounded border-2 border-[#00a99d] peer-checked:bg-[#e0f7f5] flex items-center justify-center"
+      //     onChange={(e) => {
+      //       if (e.target.checked) {
+      //         setSelectedRows(data.map((row) => row._id));
+      //       } else {
+      //         setSelectedRows([]);
+      //       }
+      //     }}
+      //     checked={data.length > 0 && selectedRows.length === data.length}
+      //   />
+      // ),
+      // cell: (row) => (
+      //   <input
+      //     type="checkbox"
+      //     checked={selectedRows.includes(row._id)}
+      //     className="w-5 h-5 rounded border-2 border-[#00a99d] peer-checked:bg-[#e0f7f5] flex items-center justify-center"
+      //     onChange={(e) => {
+      //       if (e.target.checked) {
+      //         setSelectedRows([...selectedRows, row._id]);
+      //       } else {
+      //         setSelectedRows(selectedRows.filter((id) => id !== row._id));
+      //       }
+      //     }}
+      //   />
+      // ),
     },
     { id: "leaveType", header: "Leave Type" },
-    { id: "reason", header: "Reason" },
-    { id: "employeeId", header: "Emp_Id" },
+    { id: "reason", header: "Reason",
+      cell:(row)=><p>{row.reason.slice(0 , 16)}...</p>
+     },
+    // { id: "employeeId", header: "Emp_Id" },
     { id: "name", header: "Name" },
-    { id: "_id", header: "Leave Id" },
+    // { id: "_id", header: "Leave Id" },
     { id: "from", header: "From" },
     { id: "to", header: "To" },
-    { id: "status", header: "Status" },
-    { id: "Aboutemployee", header: "Approved By" },
-    { id: "Aboutemployee", header: "Approved At" },
+    { id: "status", header: "Status"  , 
+      cell:(row)=><p style={{
+        backgroundColor:statusColors[row.status] , 
+        padding:4 , 
+        paddingRight:8 , 
+        padddingLeft:8 , 
+        fontWeight:"bold",
+        borderRadius:12
+      }}>{row.status}</p>
+
+    },
+    // {id:"email" ,header:"Email"},
+    // { id: "Aboutemployee", header: "Approved By" },
+    // { id: "Aboutemployee", header: "Approved At" },
       {
            id: "actions",
            header: "Actions",
-           cell: (row) => (
-             <div className="flex gap-5 ml-2">
-               <button onClick={() => handleAproved(row._id, row.Key)}>
-                <FcApproval className="w-6 h-6" />
+           cell: (row) => {
+            
+            const isDisabled = row.status==="Approved" || row.status==="Cancelled"
+            
+            return(
+             <div className="flex gap-5">
+               <button disabled={isDisabled} onClick={() => handleAproved(row._id, row.Key,row.email)} > 
+                <FcApproval className={`w-6 h-6 ${isDisabled && "text-green-200 opacity-25"}`} />
 
                  {/* <img src={Delete} className="w-5 h-6" /> */}
                </button>
-               <button onClick={() => handleRemove(row._id,row.Key)}>
-                <MdCancel className="w-6 h-6 text-red-600"></MdCancel>
+               <button disabled={isDisabled} onClick={() => handleRemove(row._id,row.Key)} >
+                <MdCancel className={`w-6 h-6 ${isDisabled?"text-red-300" :"text-red-600"} `}></MdCancel>
 
                  {/* <img src={Book} alt="Edit" className="w-7 h-7" /> */}
                </button>
              </div>
-           ),
+           )},
          },
 
    
@@ -230,10 +271,11 @@ function LeavesApply() {
   };
 
 
-  // function Color(col)
+  // function setColors(col)
   // {
   //   setColor(col)
 
+  // console.log("..............",colorstate)
 
   // }
 
@@ -243,43 +285,43 @@ function LeavesApply() {
         <p className="text-2xl">Manage Employee</p>
         <div className="flex gap-3 mt-2 items-center flex-wrap">
                <select
-        onChange={handleChange}
+        // onChange={handleChange}
         name="leavstatus"
-        className="border-2 rounded-lg p-2 border-[#00A99D] text-">
+        className={`border-2 rounded-lg p-2 border-[#00A99D] text-${colorstate}`}>
             <option value="">Leave status</option>
-            <option value="Pending" className="text-orange-500" >Pending</option>
+            <option value="Pending" className="text-orange-500" onChange={handleChange}>Pending</option>
             <option value="Approved" className="text-green-600">Approved</option>
             <option value="Cancle" className="text-red-600">Cancle</option>
             
 
         </select>
           <Search onChange={handleSearchChange} />
-          <ExportPDF elementId="employee" fileName="employees.pdf" />
-          <button
+          {/* <ExportPDF elementId="employee" fileName="employees.pdf" /> */}
+          {/* <button
             onClick={handleAdd}
             className="font-200 bg-[#00A79B] text-white border-2 rounded-xl border-[#00A79B] py-2 px-3"
           >
             Add Employee +
-          </button>
+          </button> */}
         </div>
       </div>
 
-      <div className="flex justify-between px-5 py-3">
+      <div className="flex justify-between px-5 py-1">
         <p>Total {totalResponses} Responses</p>
         <p>No filters applied</p>
      
-        <button
+        {/* <button
           onClick={handleCheckboxDelete}
           className="font-200 flex gap-2 bg-white text-[#e71818] border-2 rounded-xl border-[#e71818] py-2 px-3"
         >
           Delete Selections
           <img src={Delete} alt="delete" className="w-5 h-5" />
-        </button>
+        </button> */}
       </div>
 
       <Table data={data} columns={columns} />
 
-      <div className="flex justify-center items-center mt-10 gap-4 px-7 flex-row">
+      <div className="flex justify-center items-center  gap-4 px-7 flex-row">
         <span className="text-lg flex-1 text-[#444444] font-medium">
           Page {page} of {totalPages}
         </span>
